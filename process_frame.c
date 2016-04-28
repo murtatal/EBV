8b384dd62358ac7132ae3a08bcd5ac71611185c7
oscar <oscar@oscar.(none)>
2016-04-28 19:41:21 +0200

    save übung

--- process_frame.c
+++ process_frame.c
@@ -11,6 +11,8 @@
 #include "template.h"
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
+
 
 #define IMG_SIZE NUM_COLORS*OSC_CAM_MAX_IMAGE_WIDTH*OSC_CAM_MAX_IMAGE_HEIGHT
 
@@ -18,6 +20,89 @@ const int nc = OSC_CAM_MAX_IMAGE_WIDTH;
 const int nr = OSC_CAM_MAX_IMAGE_HEIGHT;
 
 int TextColor;
+float bgrImg[IMG_SIZE];
+const float avgFac = 0.99;
+const int Border = 1;
+const int frgLimit = 100;
+
+
+
+void ChangeDetection() {
+int r, c;
+//set result buffer to zero
+memset(data.u8TempImage[THRESHOLD], 0, IMG_SIZE);
+//loop over the rows
+for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
+//loop over the columns
+for(c = Border; c < (nc-Border); c++) {
+float pImg = data.u8TempImage[SENSORIMG][r+c];
+float pBgr = bgrImg[r+c];
+float Dif = fabs(pImg-pBgr);
+//if the difference is larger than threshold value
+if(Dif > data.ipc.state.nThreshold) {
+//set pixel value to 255 in THRESHOLD
+data.u8TempImage[THRESHOLD][r+c] = 255;
+//increase foreground counter
+data.u8TempImage[INDEX1][r+c]++;
+//check whether limit is reached
+if(data.u8TempImage[INDEX1][r+c] == frgLimit) {
+//set pixel to background
+bgrImg[r+c] = (float) data.u8TempImage[SENSORIMG][r+c];
+//reset foreground counter
+data.u8TempImage[INDEX1][r+c] = 0;
+}
+
+}else{
+	bgrImg[r+c] = avgFac*bgrImg[r+c] + (1-avgFac)*(float)
+	data.u8TempImage[SENSORIMG][r+c];
+	// set value for display
+	data.u8TempImage[BACKGROUND][r+c] = (unsigned char)
+	bgrImg[r+c];
+	//set foreground counter to zero
+	data.u8TempImage[INDEX1][r+c] = 1;
+	}
+
+		}
+
+void SetBackground() {
+	int r, c;
+	//loop over the rows
+	for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
+	//loop over the columns
+	for(c = Border; c < (nc-Border); c++) {
+	bgrImg[r+c] = (float) data.u8TempImage[SENSORIMG][r+c];
+	}
+	}
+	memset(data.u8TempImage[INDEX1], 0, IMG_SIZE);
+
+	}
+
+void Erode_3x3(int InIndex, int OutIndex) {
+int c, r;
+for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
+for(c = Border; c < (nc-Border); c++) {
+unsigned char* p = &data.u8TempImage[InIndex][r+c];
+data.u8TempImage[OutIndex][r+c] =
+*(p-nc-1) & *(p-nc) & *(p-nc+1) &
+*(p-1)
+& *p
+& *(p+1)
+&
+*(p+nc-1) & *(p+nc) & *(p+nc+1);
+}
+}
+}
+
+void Dilate_3x3(int InIndex, int OutIndex) {
+	int c, r;
+	for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
+	for(c = Border; c < (nc-Border); c++) {
+	unsigned char* p = &data.u8TempImage[InIndex][r+c];
+	data.u8TempImage[OutIndex][r+c] =
+	*(p-nc-1) |*(p-nc) & *(p-nc+1) |*(p-1)|*p|*(p+1)|*(p+nc-1) |*(p+nc) |*(p+nc+1);
+	}
+	}
+	}
 
 
 void ResetProcess()
@@ -27,12 +112,13 @@ void ResetProcess()
 		TextColor = MAGENTA;
 	else
 		TextColor = CYAN;
+
+	SetBackground();
 }
 
 
-void ProcessFrame()
-{
-	char Text[] = "hallo world";
+void ProcessFrame(){
+	//char Text[] = "hallo world";
 	//initialize counters
 	if(data.ipc.state.nStepCounter == 1) {
 		//use for initialization; only done in first step
@@ -40,16 +126,19 @@ void ProcessFrame()
 		TextColor = CYAN;
 	} else {
 		//example for copying sensor image to background image
-		memcpy(data.u8TempImage[BACKGROUND], data.u8TempImage[SENSORIMG], IMG_SIZE);
+		//memcpy(data.u8TempImage[BACKGROUND], data.u8TempImage[SENSORIMG], IMG_SIZE);
+		ChangeDetection();
+		Erode_3x3(THRESHOLD, INDEX0);
+		Dilate_3x3(INDEX0, THRESHOLD);
 
 		//example for drawing output
 		//draw line
-		DrawLine(10, 100, 200, 20, RED);
+		//DrawLine(10, 100, 200, 20, RED);
 		//draw open rectangle
-		DrawBoundingBox(20, 10, 50, 40, false, GREEN);
+		//DrawBoundingBox(20, 10, 50, 40, false, GREEN);
 		//draw filled rectangle
-		DrawBoundingBox(80, 100, 110, 120, true, BLUE);
-		DrawString(200, 200, strlen(Text), TINY, TextColor, Text);
+		//DrawBoundingBox(80, 100, 110, 120, true, BLUE);
+		//DrawString(200, 200, strlen(Text), TINY, TextColor, Text);
 	}
 }
 
